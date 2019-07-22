c0f4ef9d2c0c17c90da34fd0e8f203290ff74ee9
aliakh <liakh.aliaksandr@gmail.com>
2019-07-22 19:05:22 +0300

    Update

---
+++ demo-spring-aop/demo-spring-aop-aspectj-load-time-weaving/src/main/java/demo/AspectJApplication.java
@@ -0,0 +1,44 @@
+package demo;
+
+import demo.service.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+import org.springframework.util.ClassUtils;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+//http://blog.kezhuw.name/2017/08/31/spring-aspectj-load-time-weaving/
+//https://www.credera.com/blog/technology-insights/open-source-technology-insights/aspect-oriented-programming-in-spring-boot-part-3-setting-up-aspectj-load-time-weaving/
+@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
+public class AspectJApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AspectJApplication.class);
+
+    @Autowired
+    private DemoService demoService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspectJApplication.class);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            LOGGER.info("Service: " + demoService);
+            LOGGER.info("Class: " + demoService.getClass());
+            LOGGER.info("Implemented interfaces: " + Arrays.toString(ClassUtils.getAllInterfaces(demoService)));
+            LOGGER.info("Superclass: " + demoService.getClass().getSuperclass());
+            LOGGER.info("Sum: " + demoService.add(2,3));
+        };
+    }
+}
+
+//https://github.com/jwilsoncredera/spring-aop-blog
+//http://foat.me/articles/java-aspects-using-spring-aop-and-aspectj/
