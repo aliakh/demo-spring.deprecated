02cd072d68deb94c36197a6558d1d2038adfa6ea
aliakh <liakh.aliaksandr@gmail.com>
2019-07-22 15:55:18 +0300

    Update

--- demo-spring-aop/demo-spring-aop-jdk-proxy/src/main/java/demo/DemoService.java
+++ demo-spring-aop/demo-spring-aop-jdk-proxy/src/main/java/demo/DemoService.java
@@ -1,9 +1,5 @@
 package demo;
 
-import demo.shared.Todo;
-
-import java.util.List;
-
 public interface DemoService {
 
     int add(int a, int b);
--- demo-spring-aop/demo-spring-aop-jdk-proxy/src/main/java/demo/DemoServiceImpl.java
+++ demo-spring-aop/demo-spring-aop-jdk-proxy/src/main/java/demo/DemoServiceImpl.java
@@ -1,14 +1,6 @@
 package demo;
 
-import demo.shared.Todo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Service
 public class DemoServiceImpl implements DemoService {
